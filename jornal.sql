ed2c31bbabb04b3397a104497d612833835fedf0
marciogriin <107067032+marciogriin@users.noreply.github.com>
2022-06-14 15:20:32 -0300

    Add files via upload
    atualização novas funções

--- jornal.sql
+++ jornal.sql
@@ -1,14 +1,24 @@
 create database jornal;
 
 create table categorias(
-id int primary key,
+id int primary key auto_increment,
 nome varchar(65)
 );
 
 create table noticias(
-id int primary key,
+id int primary key auto_increment,
 nome varchar (65),
 texto text,
 id_categoria int,
 Foreign key (id_categoria) REFERENCES `categorias` (`id`)
 );
+
+select * from categorias;
+
+insert into categorias (id, nome) VALUES (1, 'esportes');
+
+update;
+
+
+
+
--- projeto.py
+++ projeto.py
@@ -1,38 +1,65 @@
+import mysql.connector
+
+
+
 def listar_categoria():		#1
-	print('Lista categoria')
+	db_connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="jornal")
+	cursor = db_connection.cursor()
+
+	sql = ("SELECT id, nome FROM categorias")
+	cursor.execute(sql)
+
+	print('{:^10}|{:^10}'.format('Id', 'Nome'))
+	for (id, nome) in cursor:
+	  print('{:^10}|{:^10}'.format(id, nome))
+	print("\n")
+
+	cursor.close()
+	db_connection.close()
 
 def	inserir_categoria():	#2
-	print('Inserir categoria')
+	db_connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="jornal")
+	cursor = db_connection.cursor()
+
+	nome = input("inserir nome:")
+
+	sql = "INSERT INTO categorias (nome) VALUES (%s)"
+	values = (nome,)
+	cursor.execute(sql, values)
+
+	cursor.close()
+	db_connection.commit()
+	db_connection.close()
 
 def	alterar_categoria():	#3
-	print('Alterar categoria')
+	print('\n Alterar categoria \n')
 	
 def apagar_categoria():		#4
-	print('Apagar categoria')
+	print('\n Apagar categoria \n')
 
 def listar_noticia():		#5
-	print('Listar noticia')
+	print('\n Listar noticia \n')
 	
 def inserir_noticia():		#6
-	print('Inserir noticia')
+	print('\n Inserir noticia \n')
 	
 def alterar_noticia():		#7
-	print('Alterar noticia')
+	print('\n Alterar noticia \n')
 	
 def apagar_noticia():		#8
-	print('Apagar noticia')
+	print('\n Apagar noticia \n')
 
 while True:
 	print('''1 Listar categorias
-	2 Inserir categoria
-	3 Alterar categoria
-	4 Apagar categoria
-	5 Listar noticias
-	6 Inserir noticia
-	7 Alterar noticia
-	8 Apagar noticia
-	9 Sair''')
-	opcao = input(escolha uma opcao)
+2 Inserir categoria
+3 Alterar categoria
+4 Apagar categoria
+5 Listar noticias
+6 Inserir noticia
+7 Alterar noticia
+8 Apagar noticia
+9 Sair''')
+	opcao = int(input('escolha uma opcao:'))
 	if opcao == 1:
 		listar_categoria()
 	
